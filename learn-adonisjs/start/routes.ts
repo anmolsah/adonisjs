97e9d82bc2e96653a206fd36a26d89aefef12d25
Anmol Sah <kumarxxx242424@gmail.com>
2025-05-10 01:08:31 +0530

    feat: add movies page and update routes for movie display

--- learn-adonisjs/start/routes.ts
+++ learn-adonisjs/start/routes.ts
@@ -9,8 +9,18 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home').as('home')
 
-router.get('/movies', async (ctx) => {
-  return 'my movies'
-})
+router
+  .get('/movies/my-awesome-movie', async (ctx) => {
+    return ctx.view.render('pages/movies', { movie: 'my awesome movies' })
+  })
+  .as('movies.show')
+
+//   router.get('/movies', () => {}).as('movies.index')
+// router.get('/movies/my-awesome-movie', () => {}).as('movies.show')
+// router.get('/movies/create', () => {}).as('movies.create')
+// router.post('/movies', () => {}).as('movies.store')
+// router.get('/movies/my-awesome-movie/edit', () => {}).as('movies.edit')
+// router.put('/movies/my-awesome-movie', () => {}).as('movies.update')
+// router.delete('/movies/my-awesome-movie', () => {}).as('movies.destroy')
